37c3e3aa36720e142fc9bb5bfdaf53ddb2936b54
Uditha Chathuranga <udithchathuranga@Udithas-MacBook-M4.local>
2025-05-29 21:53:47 +0530

    remove unnessary

--- app/quection3..ts
+++ app/quection3..ts
@@ -1,7 +1,7 @@
 export default function  twoSum (numbers: number[], target: number): number[] {
 const arr : number[] = [];
  for (let i = 0; i < numbers.length; i++) {
- for (let j =i+1; i < numbers.length; j++) {
+ for (let j =i+1; j < numbers.length; j++) {
       if(numbers[i]+numbers[j] == target){
           arr[0] =i;
           arr[1] =j;
